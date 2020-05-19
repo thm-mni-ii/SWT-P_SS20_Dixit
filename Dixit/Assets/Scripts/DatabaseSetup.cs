c61f387e16d9cf07e9a9c7faf25d22b160d914c8
Nicole Gerbershagen <nicole.gerbershagen@mni.thm.de>
2020-05-19 19:35:59 +0200

    added test scene and script

---
+++ Dixit/Assets/Scripts/DatabaseSetup.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class DatabaseSetup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set up the Editor before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://swt-p-ss20-profcollector.firebaseio.com/");
+
+        // Get the root reference location of the database.
+        DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+}
---
+++ Dixit/Assets/Scripts/Testi.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Database;
+using UnityEngine.UI;
+
+public class Testi : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text m_SomeText;
+   public void DoSomething() {
+        FirebaseDatabase.DefaultInstance
+       .GetReference("users")
+       .GetValueAsync().ContinueWith(task => {
+           if (task.IsFaulted)
+           {
+              // Handle the error...
+          }
+           else if (task.IsCompleted)
+           {
+               DataSnapshot snapshot = task.Result;
+               // Do something with snapshot...
+               m_SomeText.text = (string)snapshot.Child("/Dren/email").GetValue(false);
+          }
+       });
+
+    }
+}

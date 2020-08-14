bc547ab12bfd6982af0c72c9269fb8aee9873ed5
so-9604 <sophia.heck@mni.thm.de>
2020-08-14 20:32:37 +0200

    added equality test for numbers in digits and german words

--- Dixit/Assets/Scripts/Tests/GameManagerTest.cs
+++ Dixit/Assets/Scripts/Tests/GameManagerTest.cs
@@ -169,5 +169,43 @@ namespace Tests
             }
             yield return new WaitForSeconds(gameManager.timerToCheckResults);
         }
+
+        [Test]
+        public void NumberToWordTest()
+        {   
+            Assert.AreEqual(gameManager.numberToGermanWord(0), "null");
+            Assert.AreEqual(gameManager.numberToGermanWord(1), "eins");
+            Assert.AreEqual(gameManager.numberToGermanWord(9), "neun");
+            Assert.AreEqual(gameManager.numberToGermanWord(10), "zehn");
+            Assert.AreEqual(gameManager.numberToGermanWord(11), "elf");
+            Assert.AreEqual(gameManager.numberToGermanWord(12), "zwölf");
+            Assert.AreEqual(gameManager.numberToGermanWord(13), "dreizehn");
+            Assert.AreEqual(gameManager.numberToGermanWord(16), "sechzehn");
+            Assert.AreEqual(gameManager.numberToGermanWord(19), "neunzehn");
+            Assert.AreEqual(gameManager.numberToGermanWord(20), "zwanzig");
+            Assert.AreEqual(gameManager.numberToGermanWord(21), "einundzwanzig");
+            Assert.AreEqual(gameManager.numberToGermanWord(32), "zweiunddreißig");
+            Assert.AreEqual(gameManager.numberToGermanWord(40), "vierzig");
+            Assert.AreEqual(gameManager.numberToGermanWord(67), "siebenundsechzig");
+            Assert.AreEqual(gameManager.numberToGermanWord(75), "fünfundsiebzig");
+            Assert.AreEqual(gameManager.numberToGermanWord(99), "neunundneunzig");
+            Assert.AreEqual(gameManager.numberToGermanWord(100), "einhundert");
+            Assert.AreEqual(gameManager.numberToGermanWord(234), "zweihundertvierunddreißig");
+            Assert.AreEqual(gameManager.numberToGermanWord(999), "neunhundertneunundneunzig");
+        }
+
+        [Test]
+        public void EqualityTest()
+        {
+            Assert.IsTrue(gameManager.AnswersAreEqual("aussage", "Aussage"));
+
+            Assert.IsTrue(gameManager.AnswersAreEqual("8 Bit", "acht bit"));
+
+            Assert.IsTrue(gameManager.AnswersAreEqual("Es sind 3", "Es sind drei"));
+
+            Assert.IsTrue(gameManager.AnswersAreEqual("fünfunddreißig grad", "35 grad"));
+
+            Assert.IsTrue(gameManager.AnswersAreEqual("Beinhaltet 18 Liter", "beinhaltet achtzehn Liter"));
+        }
     }
 }

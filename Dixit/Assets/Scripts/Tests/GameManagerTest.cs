73200b47c799c0d744c2df3a582ab0dfc1a30ee1
Benjamin Siderius-Manning <64083237+b3s1r@users.noreply.github.com>
2020-08-25 21:34:52 +0200

    Cleanup GameManager (#101)
    * Moved some methods to the new utils class
    
    * Commented the now public methods

--- Dixit/Assets/Scripts/Tests/GameManagerTest.cs
+++ Dixit/Assets/Scripts/Tests/GameManagerTest.cs
@@ -29,7 +29,7 @@ namespace Tests
         {
             for (int m = 0; m < 100; m++)
             {
-                var randomQuestions = gameManager.GetRandomQuestionIdxArray(30);
+                var randomQuestions = Utils.GetRandomQuestionIdxArray(30, gameManager.numberOfRounds);
 
                 for (int i = 0; i < randomQuestions.Length; i++)
                 {

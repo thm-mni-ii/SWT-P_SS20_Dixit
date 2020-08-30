43093649637ffa98148c5dc361194a6a7600f749
Sophia Heck <sophia.heck@mni.thm.de>
2020-08-30 16:33:54 +0200

    split notification test and added database test

--- Dixit/Assets/Scripts/Tests/NotificationTest.cs
+++ Dixit/Assets/Scripts/Tests/NotificationTest.cs
@@ -20,7 +20,7 @@ namespace Tests
         [UnitySetUp]
         public IEnumerator Setup()
         {
-             if(!serverStarted)
+            if(!serverStarted)
             {
                 var gso = MonoBehaviour.Instantiate(Resources.Load("Prefabs/NetworkManager")) as GameObject;
                 gameServer = gso.GetComponent<GameServer>();
@@ -50,7 +50,7 @@ namespace Tests
         }
 
         [UnityTest]
-        public IEnumerator SendNotifications()
+        public IEnumerator ShowAndColorTest()
         {
             yield return new WaitForSeconds(1f);
             
@@ -59,95 +59,15 @@ namespace Tests
                                    "1","testBAD")
             );
 
-            yield return new WaitForSeconds(0.5f);
-            var found = false;
-            foreach (Transform notification in notificationCanvas.transform)
-            {
-                var canvas = notification.GetChild(0).gameObject;
-                var tmp = canvas.GetComponent<TMP_Text>();
-
-                if(tmp.text == "testBAD")
-                {
-                    found = true;
-                    Assert.AreEqual(notificationSystem.badColor,tmp.color);
-
-                    notification.GetComponent<NotificationCanvas>().showLong();
-                    yield return new WaitForSeconds(0.5f);
-                    Assert.AreEqual("1", tmp.text);
-
-                    notification.GetComponent<NotificationCanvas>().showShort();
-                    yield return new WaitForSeconds(0.5f);
-                    Assert.AreEqual("testBAD", tmp.text);
-
-                }
-                
-            }
-
-            Assert.IsTrue(found);
-
             Utils.GetPlayers().First().TargetSendNotification (
                 new Notification ( Notification.NotificationTypes.good,
                                    "2","testGOOD")
             );
 
-            yield return new WaitForSeconds(0.5f);
-
-            found = false;
-            foreach (Transform notification in notificationCanvas.transform)
-            {
-                var canvas = notification.GetChild(0).gameObject;
-                var tmp = canvas.GetComponent<TMP_Text>();
-
-                if(tmp.text == "testGOOD")
-                {
-                    found = true;
-                    Assert.AreEqual(notificationSystem.goodColor,tmp.color);
-
-                    notification.GetComponent<NotificationCanvas>().showLong();
-                    yield return new WaitForSeconds(0.5f);
-                    Assert.AreEqual("2", tmp.text);
-
-                    notification.GetComponent<NotificationCanvas>().showShort();
-                    yield return new WaitForSeconds(0.5f);
-                    Assert.AreEqual("testGOOD", tmp.text);
-
-                }
-                
-            }
-
-            Assert.IsTrue(found);
-
             Utils.GetPlayers().First().TargetSendNotification (
                 new Notification ( Notification.NotificationTypes.regular,
                                    "3","testREG")
             );
-
-            yield return new WaitForSeconds(0.5f);
-
-            found = false;
-            foreach (Transform notification in notificationCanvas.transform)
-            {
-                var canvas = notification.GetChild(0).gameObject;
-                var tmp = canvas.GetComponent<TMP_Text>();
-
-                if(tmp.text == "testREG")
-                {
-                    found = true;
-                    Assert.AreEqual(notificationSystem.regularColor,tmp.color);
-
-                    notification.GetComponent<NotificationCanvas>().showLong();
-                    yield return new WaitForSeconds(0.5f);
-                    Assert.AreEqual("3", tmp.text);
-
-                    notification.GetComponent<NotificationCanvas>().showShort();
-                    yield return new WaitForSeconds(0.5f);
-                    Assert.AreEqual("testREG", tmp.text);
-
-                }
-                
-            }
-
-            Assert.IsTrue(found);
 
             Utils.GetPlayers().First().TargetSendNotification (
                 new Notification ( Notification.NotificationTypes.warning,
@@ -155,41 +75,115 @@ namespace Tests
             );
 
             yield return new WaitForSeconds(0.5f);
-
-            found = false;
+            var found = 0;
             foreach (Transform notification in notificationCanvas.transform)
             {
                 var canvas = notification.GetChild(0).gameObject;
                 var tmp = canvas.GetComponent<TMP_Text>();
 
+                if(tmp.text == "testBAD")
+                {
+                    found++;
+                    Assert.AreEqual(notificationSystem.badColor,tmp.color);
+                }
+
+                if(tmp.text == "testGOOD")
+                {
+                    found++;
+                    Assert.AreEqual(notificationSystem.goodColor,tmp.color);
+                }
+
+                if(tmp.text == "testREG")
+                {
+                    found++;
+                    Assert.AreEqual(notificationSystem.regularColor,tmp.color);
+                }
+
                 if(tmp.text == "testWARNING")
                 {
-                    found = true;
+                    found++;
                     Assert.AreEqual(notificationSystem.warningColor,tmp.color);
-
-                    notification.GetComponent<NotificationCanvas>().showLong();
-                    yield return new WaitForSeconds(0.5f);
-                    Assert.AreEqual("4", tmp.text);
-
-                    notification.GetComponent<NotificationCanvas>().showShort();
-                    yield return new WaitForSeconds(0.5f);
-                    Assert.AreEqual("testWARNING", tmp.text);
-
                 }
                 
             }
 
-            Assert.IsTrue(found);
+            Assert.AreEqual(4, found);
+        }
 
-            //test for more the five notifications the oldest is deleted
 
+        [UnityTest]
+        public IEnumerator ShowLongTest()
+        {
+            yield return new WaitForSeconds(0.5f);
+            
             Utils.GetPlayers().First().TargetSendNotification (
-                new Notification ( Notification.NotificationTypes.warning,
-                                   "5","test")
+                new Notification ( Notification.NotificationTypes.regular,
+                                   "long explanation","short")
+            );
+
+            yield return new WaitForSeconds(0.5f);
+            
+            foreach (Transform notification in notificationCanvas.transform)
+            {
+                var canvas = notification.GetChild(0).gameObject;
+                var tmp = canvas.GetComponent<TMP_Text>();
+
+                if(tmp.text == "short")
+                {
+                    notification.GetComponent<NotificationCanvas>().showLong();
+                    yield return new WaitForSeconds(0.5f);
+                    Assert.AreEqual("long explanation", tmp.text);
+
+                    notification.GetComponent<NotificationCanvas>().showShort();
+                    yield return new WaitForSeconds(0.5f);
+                    Assert.AreEqual("short", tmp.text);
+
+                }
+                
+            }
+        }
+
+        [UnityTest]
+        public IEnumerator ShowFiveTest()
+        {
+             //test for more the five notifications the oldest is deleted
+            Utils.GetPlayers().First().TargetSendNotification (
+                new Notification ( Notification.NotificationTypes.regular,
+                                   "1","testOne")
             );
 
             Utils.GetPlayers().First().TargetSendNotification (
-                new Notification ( Notification.NotificationTypes.warning,
+                new Notification ( Notification.NotificationTypes.regular,
+                                   "2","test")
+            );
+            Utils.GetPlayers().First().TargetSendNotification (
+                new Notification ( Notification.NotificationTypes.regular,
+                                   "3","test")
+            );
+
+            Utils.GetPlayers().First().TargetSendNotification (
+                new Notification ( Notification.NotificationTypes.regular,
+                                   "4","test")
+            );
+
+            Utils.GetPlayers().First().TargetSendNotification (
+                new Notification ( Notification.NotificationTypes.regular,
+                                   "5","test")
+            );
+
+            yield return new WaitForSeconds(1f);
+
+            var found = false;
+            foreach (Transform notification in notificationCanvas.transform)
+            {
+                if(notification.GetChild(0).gameObject.GetComponent<TMP_Text>().text == "testOne")
+                    found = true;                
+            }
+
+            Assert.IsTrue(found);
+
+            Utils.GetPlayers().First().TargetSendNotification (
+                new Notification ( Notification.NotificationTypes.regular,
                                    "6","test")
             );
 
@@ -203,7 +197,7 @@ namespace Tests
             }
 
             Assert.IsFalse(found);
-           
+
         }
     }
 }

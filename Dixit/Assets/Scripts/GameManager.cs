0b6e32ae9fdade332be16fc5f924aad874f09bcd
Nicole Gerbershagen <nicole.gerbershagen@mni.thm.de>
2020-08-30 17:56:09 +0200

    removed auto resizing

--- Dixit/Assets/Scripts/GameManager.cs
+++ Dixit/Assets/Scripts/GameManager.cs
@@ -57,27 +57,6 @@ public class GameManager : NetworkBehaviour
     /// \author SWT-P_SS_20_Dixit
     public DisplayManager displayManager;
 
-    /// <summary>
-    /// PlayerCanvasNames Textgroup to be resized similarly
-    /// </summary>
-    /// \author SWT-P_SS_20_Dixit
-    public TextResizer PlayerCanvasNames;
-    /// <summary>
-    /// PlayerCanvasScores Textgroup to be resized similarly
-    /// </summary>
-    /// \author SWT-P_SS_20_Dixit
-    public TextResizer PlayerCanvasScores;
-    /// <summary>
-    /// ScoreResultsNames Textgroup to be resized similarly
-    /// </summary>
-    /// \author SWT-P_SS_20_Dixit
-    public TextResizer ScoreResultsNames;
-    /// <summary>
-    /// ScoreResultsScores Textgroup to be resized similarly
-    /// </summary>
-    /// \author SWT-P_SS_20_Dixit
-    public TextResizer ScoreResultsScores;
-
     private enum Phase
     {
         WriteAnswer,
@@ -520,8 +499,6 @@ public class GameManager : NetworkBehaviour
             displayManager.RpcUpdatePlayerCanvasEntry(idx, player, playerPoints);
             idx--;
         }
-        PlayerCanvasNames.RpcAssimilateSize();
-        PlayerCanvasScores.RpcAssimilateSize();
     }
 
     /// <summary>
@@ -554,8 +531,6 @@ public class GameManager : NetworkBehaviour
 
             idx++;
         }
-        ScoreResultsNames.RpcAssimilateSize();
-        ScoreResultsScores.RpcAssimilateSize();
     }
 
     private void GetPoints(UInt32 player, int newPoints)
